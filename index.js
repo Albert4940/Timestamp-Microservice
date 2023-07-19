d9ad38b86e016dba5fd99aef3ba6ea8642d9da37
Albert4940 <dorcealbertmary@gmail.com>
2023-07-19 02:00:12 -0400

    TM-5: return a index.html file for / route

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
+var path = require("path");
 let app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.json("Starting");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 app.listen(port, () => console.log(`Server runnig on port ${port}`));
